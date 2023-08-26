52597a4d9b4e5fe5f62a2a57d04c659737841ae3
MaxOhn <ohn.m@hotmail.de>
2023-08-26 12:23:01 +0200

    add phantom generic to ArchivedIdOption

--- src/rkyv_util/id/map.rs
+++ src/rkyv_util/id/map.rs
@@ -1,5 +1,6 @@
 use std::{
     fmt::{Debug, Formatter, Result as FmtResult},
+    marker::PhantomData,
     num::NonZeroU64,
     ptr::addr_of_mut,
 };
@@ -27,12 +28,37 @@ use twilight_model::id::Id;
 /// ```
 pub struct IdRkyvMap;
 
-#[derive(Copy, Clone, PartialEq, Eq)]
-pub struct ArchivedIdOption {
+pub struct ArchivedIdOption<T> {
     inner: u64,
+    phantom: PhantomData<fn(T) -> T>,
 }
 
-impl ArchivedIdOption {
+impl<T> Clone for ArchivedIdOption<T> {
+    fn clone(&self) -> Self {
+        Self {
+            inner: self.inner,
+            phantom: PhantomData,
+        }
+    }
+}
+
+impl<T> Copy for ArchivedIdOption<T> {}
+
+impl<T> PartialEq for ArchivedIdOption<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.inner == other.inner
+    }
+}
+
+impl<T> Eq for ArchivedIdOption<T> {}
+
+impl<T> PartialEq<Option<Id<T>>> for ArchivedIdOption<T> {
+    fn eq(&self, other: &Option<Id<T>>) -> bool {
+        self.to_id_option() == *other
+    }
+}
+
+impl<T> ArchivedIdOption<T> {
     pub fn to_nonzero_option(self) -> Option<NonZeroU64> {
         if self.inner != 0 {
             // SAFETY: NonZero types have the same memory layout and bit patterns as
@@ -45,7 +71,7 @@ impl ArchivedIdOption {
         }
     }
 
-    pub fn to_id_option<T>(self) -> Option<Id<T>> {
+    pub fn to_id_option(self) -> Option<Id<T>> {
         self.to_nonzero_option().map(Id::from)
     }
 
@@ -54,7 +80,7 @@ impl ArchivedIdOption {
     /// # Safety
     ///
     /// - `pos` must be the position of `out` within the archive
-    pub unsafe fn resolve_from_id<T>(opt: Option<Id<T>>, out: *mut Self) {
+    pub unsafe fn resolve_from_id(opt: Option<Id<T>>, out: *mut Self) {
         let fo = addr_of_mut!((*out).inner);
 
         if let Some(id) = opt {
@@ -65,7 +91,7 @@ impl ArchivedIdOption {
     }
 }
 
-impl Debug for ArchivedIdOption {
+impl<T> Debug for ArchivedIdOption<T> {
     fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
         Debug::fmt(&self.to_nonzero_option(), f)
     }
@@ -77,7 +103,7 @@ const _: () = {
 
     use rkyv::CheckBytes;
 
-    impl<C: ?Sized> CheckBytes<C> for ArchivedIdOption {
+    impl<C: ?Sized, T> CheckBytes<C> for ArchivedIdOption<T> {
         type Error = Infallible;
 
         unsafe fn check_bytes<'a>(value: *const Self, _: &mut C) -> Result<&'a Self, Self::Error> {
@@ -87,7 +113,7 @@ const _: () = {
 };
 
 impl<T> ArchiveWith<Option<Id<T>>> for IdRkyvMap {
-    type Archived = ArchivedIdOption;
+    type Archived = ArchivedIdOption<T>;
     type Resolver = ();
 
     unsafe fn resolve_with(
@@ -106,9 +132,12 @@ impl<S: Fallible + ?Sized, T> SerializeWith<Option<Id<T>>, S> for IdRkyvMap {
     }
 }
 
-impl<D: Fallible + ?Sized, T> DeserializeWith<ArchivedIdOption, Option<Id<T>>, D> for IdRkyvMap {
+impl<D: Fallible + ?Sized, T> DeserializeWith<ArchivedIdOption<T>, Option<Id<T>>, D> for IdRkyvMap {
     #[inline]
-    fn deserialize_with(archived: &ArchivedIdOption, _: &mut D) -> Result<Option<Id<T>>, D::Error> {
+    fn deserialize_with(
+        archived: &ArchivedIdOption<T>,
+        _: &mut D,
+    ) -> Result<Option<Id<T>>, D::Error> {
         Ok(archived.to_id_option())
     }
 }
