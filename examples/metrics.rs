658381d3ef1090c44b0b62332884ea0a437c9219
MaxOhn <ohn.m@hotmail.de>
2023-08-30 04:08:32 +0200

    added example for metrics

---
+++ examples/metrics.rs
@@ -0,0 +1,165 @@
+use std::{env, error::Error};
+
+use redlight::{config::CacheConfig, RedisCache};
+use twilight_gateway::Event;
+use twilight_model::{
+    channel::{stage_instance::PrivacyLevel, StageInstance},
+    gateway::payload::incoming::StageInstanceCreate,
+    id::Id,
+};
+
+use self::{config::Config, recorder::PrintRecorder};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // Initialize env variables from our .env file
+    dotenvy::dotenv().unwrap();
+
+    // Set the global metrics recorder.
+    // The recorder may originate from dependencies such as `https://crates.io/crates/metrics-exporter-prometheus`
+    // or a custom implementation. Checkout the documentation of the `metrics` crate for more info.
+    metrics::set_boxed_recorder(Box::new(PrintRecorder))?;
+
+    // Create our cache by using our simple `Config` which only caches stage instances
+    let url = env::var("REDIS_URL")?;
+    println!("Creating cache...");
+    let cache = RedisCache::<Config>::new(&url).await?;
+
+    // Let's create a `StageInstanceCreate` event and update our cache with it
+    let stage = StageInstance {
+        id: Id::new(1),
+        channel_id: Id::new(100),
+        guild_id: Id::new(200),
+        guild_scheduled_event_id: Default::default(),
+        privacy_level: PrivacyLevel::GuildOnly,
+        topic: Default::default(),
+    };
+
+    let event = Event::StageInstanceCreate(StageInstanceCreate(stage.clone()));
+    println!("Updating cache...");
+    cache.update(&event).await?;
+
+    // Await the metrics recorder to print its findings
+    println!("Sleeping...");
+    tokio::time::sleep(Config::METRICS_INTERVAL_DURATION).await;
+
+    // And another one
+    let stage = StageInstance {
+        id: Id::new(2),
+        ..stage.clone()
+    };
+
+    let event = Event::StageInstanceCreate(StageInstanceCreate(stage));
+    println!("Updating cache...");
+    cache.update(&event).await?;
+
+    // Await the metrics recorder again...
+    println!("Sleeping...");
+    tokio::time::sleep(Config::METRICS_INTERVAL_DURATION).await;
+
+    Ok(())
+}
+
+/// Very simple custom metrics recorder.
+/// All it does is print out whenever something happens with gauges.
+/// Chances are you want to use a dependency instead of a custom implementation.
+mod recorder {
+    use std::sync::Arc;
+
+    use metrics::{Counter, Gauge, GaugeFn, Histogram, Key, KeyName, Recorder, SharedString, Unit};
+
+    struct PrintHandle(Key);
+
+    impl GaugeFn for PrintHandle {
+        fn set(&self, value: f64) {
+            println!("Gauge set for `{}`: {}", self.0.name(), value);
+        }
+
+        fn increment(&self, _: f64) {
+            unimplemented!()
+        }
+
+        fn decrement(&self, _: f64) {
+            unimplemented!()
+        }
+    }
+
+    pub struct PrintRecorder;
+
+    impl Recorder for PrintRecorder {
+        fn describe_gauge(&self, key: KeyName, _: Option<Unit>, description: SharedString) {
+            println!(
+                "Registered key {} with description {description:?}",
+                key.as_str()
+            );
+        }
+
+        fn register_gauge(&self, key: &Key) -> Gauge {
+            Gauge::from_arc(Arc::new(PrintHandle(key.clone())))
+        }
+
+        fn describe_counter(&self, _: KeyName, _: Option<Unit>, _: SharedString) {
+            unimplemented!()
+        }
+
+        fn describe_histogram(&self, _: KeyName, _: Option<Unit>, _: SharedString) {
+            unimplemented!()
+        }
+
+        fn register_counter(&self, _: &Key) -> Counter {
+            unimplemented!()
+        }
+
+        fn register_histogram(&self, _: &Key) -> Histogram {
+            unimplemented!()
+        }
+    }
+}
+
+/// Very simple `CacheConfig` implementation to only cache stage instances
+/// and update metrics every 15 seconds.
+mod config {
+    use std::time::Duration;
+
+    use redlight::config::{CacheConfig, Cacheable, ICachedStageInstance, Ignore};
+    use rkyv::{ser::serializers::BufferSerializer, AlignedBytes, Archive, Serialize};
+    use twilight_model::channel::StageInstance;
+
+    pub struct Config;
+
+    impl CacheConfig for Config {
+        const METRICS_INTERVAL_DURATION: Duration = Duration::from_secs(15);
+
+        type Channel<'a> = Ignore;
+        type CurrentUser<'a> = Ignore;
+        type Emoji<'a> = Ignore;
+        type Guild<'a> = Ignore;
+        type Integration<'a> = Ignore;
+        type Member<'a> = Ignore;
+        type Message<'a> = Ignore;
+        type Presence<'a> = Ignore;
+        type Role<'a> = Ignore;
+        type StageInstance<'a> = CachedStageInstance; // <-
+        type Sticker<'a> = Ignore;
+        type User<'a> = Ignore;
+        type VoiceState<'a> = Ignore;
+    }
+
+    #[derive(Archive, Serialize)]
+    #[cfg_attr(feature = "validation", archive(check_bytes))]
+    pub struct CachedStageInstance;
+
+    impl<'a> ICachedStageInstance<'a> for CachedStageInstance {
+        fn from_stage_instance(_: &'a StageInstance) -> Self {
+            Self
+        }
+    }
+
+    impl Cacheable for CachedStageInstance {
+        type Serializer = BufferSerializer<AlignedBytes<0>>;
+
+        fn expire() -> Option<Duration> {
+            None
+        }
+    }
+}
