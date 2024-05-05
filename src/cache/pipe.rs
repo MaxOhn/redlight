04036eb5afa4b4eb650157fd6e8ac42194e60acb
Axyom <60527359+4xy0m@users.noreply.github.com>
2024-05-05 21:53:04 +0000

    remove member parameter validation

--- src/cache/pipe.rs
+++ src/cache/pipe.rs
@@ -83,9 +83,6 @@ impl<'c, C> Pipe<'c, C> {
     }
 
     pub(crate) fn srem(&mut self, key: RedisKey, member: impl ToRedisArgs) {
-        if member.to_redis_args().is_empty() {
-            return;
-        };
         self.pipe.srem(key, member).ignore();
     }
 
